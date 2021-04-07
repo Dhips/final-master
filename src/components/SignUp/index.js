4d7fc4fd558ba7a17a62c61a36c3582416d049c0
Dhips <dhipanraj.sundram@gmail.com>
2021-04-07 22:36:34 +0800

    Routes push 7.4

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -52,7 +52,7 @@ class SignUpFormBase extends Component {
                 })    
                 .then(() => { 
                     this.setState({ ...INITIAL_STATE});
-                    this.props.history.push(ROUTES.HOME);
+                    this.props.history.push(ROUTES.HELP);
                 })
                 .catch(error => {
                     this.setState({ error });
